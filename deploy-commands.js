4c1cdaa62ea36b9b49e807ff5f6a7ea9a95e4d3b
babapiroemok-bit <babapiroemok@gmail.com>
2026-06-18 15:46:12 +0300

    fix: node:sqlite built-in + dotenv override:false

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ override: false });
 const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ override: false });
 const { Client, GatewayIntentBits, Partials, Collection, Events, EmbedBuilder } = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 const fs = require('fs');
